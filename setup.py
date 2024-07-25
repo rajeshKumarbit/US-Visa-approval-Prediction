2ff657b7ece393b7f84e06ce943799028698f092
Rajesh Kumar <banyalrajesh20@gmail.com>
2024-07-25 10:56:29 +0530

    requirements installed

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "us_visa",
+    version = "0.0.0",
+    author = "Rajesh Kumar",
+    author_email = "banyalrajesh20@gmail.com",
+    packages = find_packages()
+    )
+    
